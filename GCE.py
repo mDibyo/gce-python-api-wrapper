da23d4cdf2e539acb3cf8e171df20134fbf99014
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-07-28 14:40:16 -0700

    Fixed firewall errors

--- GCE.py
+++ GCE.py
@@ -144,7 +144,6 @@ class GCE:
         request = self.gce_service.firewalls().insert(project=self.project_id,
                                                       body=firewall)
         response = request.execute(http=self.auth_http)
-        response = _blocking_call(self.gce_service, self.project_id, self.auth_http, response)
         print response
 
     def delete_firewall(self, firewall_name):
@@ -154,7 +153,6 @@ class GCE:
         request = self.gce_service.firewalls().delete(project=self.project_id,
                                                       firewall=firewall_name)
         response = request.execute(http=self.auth_http)
-        response = _blocking_call(self.gce_service, self.project_id, self.auth_http, response)
 
 
 def _blocking_call(gce_service, project_id, auth_http, response):
